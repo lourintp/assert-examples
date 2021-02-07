c7004aaa4bd5eea17c4da24e7e8822b1d24c72a2
Thiago Lourin <>
2021-02-07 11:32:44 -0300

    Initial project structure

---
+++ AssertExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssertExample
+//
+//  Created by Thiago de Paula Lourin on 07/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
